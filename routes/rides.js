908da00202c8374c43b34190e90dff8e60564c89
Nsuami-diela-kiese <nsuamiloick@gmail.com>
2025-07-13 17:51:04 +0200

    Update rides.js

--- routes/rides.js
+++ routes/rides.js
@@ -656,6 +656,26 @@ router.post('/ride/:id/negociation', async (req, res) => {
   }
 });
 
+router.get('/ride/:id/negociations', async (req, res) => {
+  const rideId = req.params.id;
+
+  try {
+    const result = await db.query(
+      'SELECT discussion FROM rides WHERE id = $1',
+      [rideId]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'Course introuvable' });
+    }
+
+    res.json(result.rows[0].discussion || []);
+  } catch (err) {
+    console.error("❌ Erreur récupération discussion :", err);
+    res.status(500).json({ error: 'Erreur serveur négociation' });
+  }
+});
+
 
 
 module.exports = router;
