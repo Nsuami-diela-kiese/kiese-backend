de3dc3d12485cbe9ae3ce4086e41f56dbdefacbe
Nsuami-diela-kiese <nsuamiloick@gmail.com>
2025-08-30 14:42:33 +0000

    Update driverFlags.js

--- utils/driverFlags.js
+++ utils/driverFlags.js
@@ -1,14 +1,33 @@
+// utils/driverFlags.js
 const db = require('../db');
 
-async function setBusyByPhone(phone, busy) {
-  if (!phone) return;
+/**
+ * Réserve / libère le chauffeur pour une course
+ * - NE CHANGE PAS 'available' (c'est le toggle "en ligne" coté app chauffeurs)
+ */
+async function setOnRideByPhone(phone, onRide) {
   await db.query(
-    `UPDATE drivers
-        SET on_ride = $2,
-            available = CASE WHEN $2 THEN false ELSE available END
-      WHERE phone = $1`,
-    [phone, !!busy]
+    `UPDATE drivers SET on_ride = $2 WHERE phone = $1`,
+    [phone, onRide]
   );
 }
 
-module.exports = { setBusyByPhone };
+/**
+ * Change le statut "en ligne" affiché dans l'app chauffeur
+ */
+async function setAvailableByPhone(phone, available) {
+  await db.query(
+    `UPDATE drivers SET available = $2 WHERE phone = $1`,
+    [phone, available]
+  );
+}
+
+/**
+ * Alias pratique si tu utilises "busy" dans ton code
+ * busy = on_ride
+ */
+async function setBusyByPhone(phone, busy) {
+  return setOnRideByPhone(phone, busy);
+}
+
+module.exports = { setOnRideByPhone, setAvailableByPhone, setBusyByPhone };
