041045a6c466064ac62d844aa8a56edf50c39700
Nsuami-diela-kiese <nsuamiloick@gmail.com>
2025-09-03 13:14:46 +0200

    Update otp.js

--- utils/otp.js
+++ utils/otp.js
@@ -10,25 +10,19 @@ function generateNumericCode(len = OTP_LENGTH) {
   for (let i = 0; i < len; i++) out += Math.floor(Math.random() * 10);
   return out;
 }
-
 function hashCode(code) {
   return crypto.createHash('sha256').update(`${code}:${OTP_SECRET}`).digest('hex');
 }
-
 function isE164(phone) { return /^\+\d{8,15}$/.test(phone); }
-
 function expiryDateFromNow() {
-  const d = new Date();
-  d.setMinutes(d.getMinutes() + OTP_TTL_MIN);
-  return d;
+  const d = new Date(); d.setMinutes(d.getMinutes() + OTP_TTL_MIN); return d;
 }
 
 module.exports = {
   OTP_LENGTH,
   OTP_TTL_MIN,
   generateNumericCode,
-  // ✅ alias pour compat avec l’ancien nom
-  randCode6: generateNumericCode,
+  randCode6: generateNumericCode, // alias pour compat
   hashCode,
   isE164,
   expiryDateFromNow,
